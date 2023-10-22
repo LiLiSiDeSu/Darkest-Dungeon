3dae15322772b6c0eccedb7017989108589dc825
LiLiSi <147027165+LiLiSiDeSu@users.noreply.github.com>
2023-10-23 00:41:55 +0800

    -加一下.gitignore-

--- Assets/Scripts/Other/Base Frame work/PoolNowPanel.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PoolNowPanel : InstanceBaseAuto_Mono<PoolNowPanel>
-{
-    public List<string> ListNowPanel = new List<string>();
-}
